4269f96990306cc88e1abd135f1d0ba2a5c3bd06
Hyzyr <avezberdiyew@gmail.com>
2023-06-09 01:55:09 +0500

    desktop

---
+++ js/swiper.js
@@ -0,0 +1,74 @@
+const settings = {
+  service: {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    breakpoints: {
+      940: {
+        slidesPerView: "auto",
+        spaceBetween: 35,
+      },
+    },
+  },
+  testimonials: {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    breakpoints: {
+      940: {
+        slidesPerView: "auto",
+        spaceBetween: 35,
+      },
+    },
+  },
+  projects: {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    breakpoints: {
+      940: {
+        slidesPerView: "auto",
+        spaceBetween: 45,
+      },
+    },
+  },
+};
+
+document.querySelectorAll(".swiper").forEach((swiperSlider) => {
+  const settingsKey = swiperSlider.getAttribute("data-swiper");
+  const parentElement = swiperSlider.hasAttribute("data-swiper-parent")
+    ? document.querySelector(swiperSlider.getAttribute("data-swiper-parent"))
+    : swiperSlider.parentElement;
+  const prev = parentElement.querySelector(".swiper-arrow-prev");
+  const next = parentElement.querySelector(".swiper-arrow-next");
+  const dots = parentElement.querySelector(".swiper-dots");
+  const scroll = parentElement.querySelector(".swiper-scrollbar");
+
+  new Swiper(swiperSlider, {
+    ...settings[settingsKey],
+    // // If we need pagination
+    // pagination: {
+    //   el: ".swiper-pagination",
+    // },
+
+    // // Navigation arrows
+    navigation:
+      next && prev
+        ? {
+            nextEl: next,
+            prevEl: prev,
+          }
+        : {},
+    scrollbar: scroll
+      ? {
+          el: scroll,
+          hide: false,
+          // draggable: true,
+          enabled: true,
+        }
+      : {},
+    pagination: dots
+      ? {
+          el: dots,
+          clickable: true,
+        }
+      : {},
+  });
+});
